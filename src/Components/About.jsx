90b2e07674f55ca5bb6e7604cfa9ae90eb2a97b4
FaizanHaider20083 <faizan20083@iiitd.ac.in>
2021-06-14 15:53:41 +0530

    Add files via upload

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -8,18 +8,19 @@ function About() {
           <div class="col-lg-7">
             <img
               class="img-fluid rounded mb-4 mb-lg-0"
-              src="http://placehold.it/900x400"
+              src="https://cdn.statically.io/gh/sudo-YogeshAI/ECom/Yogesh/1612802561-3192.jpg"
               alt=""
             />
           </div>
           <div class="col-lg-5">
             <h1 class="font-weight-light">About</h1>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
+            
+              <h1> Welcome to Vanguard E-Commerce</h1>
+
+             <p>
+               We provide a wide range of A-grade products ranging from Electronics to jwellery and clothing. We promise our customers state of the art products and an experience so seamless you would want to visit the site for every need.  
             </p>
+            <p>To start you might wanna view products by category or list all of them at once.</p>
           </div>
         </div>
       </div>
